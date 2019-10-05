0bbc1e9b6bcc516e576256bc47286161e3a265dd
Hank Chen <hank121314@gmail.com>
2019-10-05 09:58:11 +0800

    feat(react/webpack):add Roboto icons,ready for material design

--- webpack/webpack.config.react.prod.js
+++ webpack/webpack.config.react.prod.js
@@ -73,6 +73,7 @@ const config = merge.smart(baseConfig, {
               importLoaders: 1,
             },
           },
+          'resolve-url-loader',
           {
             loader: 'sass-loader',
             options: {
@@ -98,6 +99,7 @@ const config = merge.smart(baseConfig, {
               sourceMap: true,
             },
           },
+          'resolve-url-loader',
           {
             loader: 'sass-loader',
             options: {
@@ -105,6 +107,60 @@ const config = merge.smart(baseConfig, {
             },
           },
         ],
+      },
+      // WOFF Font
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff',
+          },
+        },
+      },
+      // WOFF2 Font
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff',
+          },
+        },
+      },
+      // TTF Font
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/octet-stream',
+          },
+        },
+      },
+      // EOT Font
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'file-loader',
+      },
+      // SVG Font
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'image/svg+xml',
+          },
+        },
+      },
+      // Common Image Formats
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
+        use: 'url-loader',
       },
     ],
   },
