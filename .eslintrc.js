636d4ccc46bd4e2da10f1aa8988f7a1003ae556d
Hank Chen <hank121314@gmail.com>
2019-10-05 16:20:53 +0800

    feat(*):add stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,27 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
     'plugin:react/recommended',
     'google',
   ],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  rules: { 'object-curly-spacing': 0, indent: [2, 2] },
+  rules: {
+    'object-curly-spacing': 0,
+    'indent': [2, 2],
+    'max-len': ['error', { code: 120, comments: 120 }],
+    '@typescript-eslint/interface-name-prefix': 1,
+  },
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
 };
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.scss';
 
 export const Index = (): ReactElement => {
   return (
-    <div className={styles.App}>
+    <div className={styles.app}>
       <Button color="primary" className={styles.title}>
         Hello React!!
       </Button>
